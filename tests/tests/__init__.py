80ddfd4d5538464fea9a8f3ec81b0651c3375a07
Daniel Pinto <d.pinto@kigroup.de>
2023-03-04 13:02:47 +0000

    Adds unit tests for signals

--- tests/tests/__init__.py
+++ tests/tests/__init__.py
@@ -2,3 +2,4 @@ from .timestamps import *
 from .softdelete import *
 from .drf import *
 from .settings import *
+from .signals import *
---
+++ tests/tests/signals.py
@@ -0,0 +1,88 @@
+from django.test import TransactionTestCase
+from timestamps import signals
+
+from tests.models import FooSoftDeletes as Foo
+
+
+@Foo.fake_me
+class TestSoftDeleteSignal(TransactionTestCase):
+    def test_pre_soft_delete(self):
+        self.signaled = False
+
+        foo = Foo()
+        foo.save()
+
+        def handler(sender, instance, **kwargs):
+            self.signaled = True
+
+            self.assertEqual(sender, Foo)
+            self.assertIs(instance, foo)
+
+        signals.pre_soft_delete.connect(handler)
+        
+        foo.soft_delete()
+        self.assertTrue(self.signaled)
+
+        signals.pre_soft_delete.disconnect(handler)
+
+    def test_post_soft_delete(self):
+        self.signaled = False
+
+        foo = Foo()
+        foo.save()
+
+        def handler(sender, instance, **kwargs):
+            self.signaled = True
+
+            self.assertEqual(sender, Foo)
+            self.assertIs(instance, foo)
+
+        signals.post_soft_delete.connect(handler)
+        
+        foo.soft_delete()
+        self.assertTrue(self.signaled)
+
+        signals.post_soft_delete.disconnect(handler)
+
+
+    def test_pre_restore(self):
+        self.signaled = False
+
+        foo = Foo()
+        foo.save()
+        foo.soft_delete()
+
+        def handler(sender, instance, **kwargs):
+            self.signaled = True
+
+            self.assertIsNotNone(instance.deleted_at)
+            self.assertEqual(sender, Foo)
+            self.assertIs(instance, foo)
+
+        signals.pre_restore.connect(handler)
+        
+        foo.restore()
+        self.assertTrue(self.signaled)
+
+        signals.pre_restore.disconnect(handler)
+
+    def test_post_restore(self):
+        self.signaled = False
+
+        foo = Foo()
+        foo.save()
+        foo.soft_delete()
+
+        def handler(sender, instance, **kwargs):
+            self.signaled = True
+
+            self.assertIsNone(instance.deleted_at)
+            self.assertEqual(sender, Foo)
+            self.assertIs(instance, foo)
+
+        signals.post_restore.connect(handler)
+        
+        foo.restore()
+        self.assertTrue(self.signaled)
+
+        signals.post_restore.disconnect(handler)
